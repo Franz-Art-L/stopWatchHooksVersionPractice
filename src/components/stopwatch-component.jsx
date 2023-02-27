b27df4b35c46d60b2ffd38285f59e880820ad878
Ishoboy <francislandia@icloud.com>
2023-02-27 18:39:27 +0800

    done converting the previous stopwatch practice project to this one using functional component with hooks, I also use the named export on the stopwatch component and have the name of the component wrapped in curly braces on the file that imported it. off to the next class component react project that I will convert into functional component with hooks.

--- src/components/stopwatch-component.jsx
+++ src/components/stopwatch-component.jsx
@@ -1,8 +1,64 @@
-const StopWatch = () => {
-    
+import React, {useState, useRef} from "react";
+
+export const StopWatch = () => {
+
+    const [timePassed, setTimePassed] = useState(0);
+
+    const timer = useRef(null);
+
+    const start = () => {
+        if(!timer.current) {
+            let startTime = new Date();
+
+            timer.current = setInterval(() => {
+                const stopTime = new Date();
+
+                setTimePassed(timePassed => stopTime - startTime + timePassed);
+
+                startTime = stopTime;
+            }, 1000)
+        }
+    }
+
+    const stop = () => {
+        window.clearInterval(timer.current);
+        timer.current = null;
+    }
+
+    const reset = () => {
+        stop();
+        setTimePassed(0);
+    }
+
     return(
-        <div></div>
+        <div>
+            <h2 className="border px-3 py-4 rounded my-3 mx-auto text-center" style={{maxWidth: "300px"}}>
+
+                {Math.floor(timePassed / 1000)}s
+
+            </h2>
+
+            <div className="d-flex justify-content-center">
+
+                <button className="btn btn-outline-primary mr-2" onClick={start}>
+
+                    start
+
+                </button>
+
+                <button className="btn btn-outline-danger mr-2" onClick={stop}>
+
+                    stop
+
+                </button>
+
+                <button className="btn btn-outline-warning" onClick={reset}>
+
+                    reset
+
+                </button>
+
+            </div>
+        </div>
     )
 }
-
-export default StopWatch;
--- src/components/template-component.jsx
+++ src/components/template-component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navbar from './navbar-component';
 import Footer from './footer.component';
-import StopWatch from './stopwatch-component';
+import {StopWatch} from './stopwatch-component';
 
 const Template = props => {
     return (
@@ -11,7 +11,7 @@ const Template = props => {
                     <div className="row overflow">
                         <div className="col-12">
                             <StopWatch/>
-                            {props.children}
+              
                         </div>
                     </div>
                 </div>
